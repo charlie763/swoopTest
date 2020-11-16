a129821adcab29980c1277e390eaf74a5ff89f49
charlie763 <cwisoff@gmail.com>
2020-11-16 14:18:06 -0700

    callbacks controller

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :oauth2_generic,
-    "swoop_4k6qx64khl0lfbe", "55e26a82b7340687a575bd623e126ced018b442bd40761e2f4fbc0692d63ca69",
+    "swoop_4k6qx64khl1er6m", "6d7ac9c247452488cd435d0676c1dec0b0f27b1b317e87bbdb92c255f58847c6",
     client_options: {
       site: 'https://auth.swoop.email', 
       token_url: '/oauth2/token',
