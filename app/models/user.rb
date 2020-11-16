fa44add508bb35b9f976a7e4f1ce34270fe6b3cb
charlie763 <cwisoff@gmail.com>
2020-11-16 14:22:17 -0700

    update user model with from omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_create do |user|
+      user.email = auth.info.email
+      if !user.password
+        user.password = Devise.friendly_token[0,20]
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'secret/index'
   devise_for :users
   root 'secret#index'
+  get '/auth/:provider/callback', to: 'callbacks#swoop'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
